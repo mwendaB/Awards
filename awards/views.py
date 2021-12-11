3f23de18d2c813a7238a53ba0bd3b7a29ddc6141
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-11 23:01:46 +0300

    create the view section

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Projects,Profile
 
 # Create your views here.
+
+def index(request):
+    projects = Projects.objects.all()
+    return render(request,'index.html',{"projects":projects})
+
