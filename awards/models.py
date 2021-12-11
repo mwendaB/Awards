31a2484317a623faf84d8d5248ca1473e664133d
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-11 22:01:40 +0300

    write the models.py

--- awards/models.py
+++ awards/models.py
@@ -23,3 +23,16 @@ class Profile(models.Model):
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
     
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    shot = CloudinaryField('images')
+    url= models.URLField(max_length=200)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default='', null=True ,related_name='author')
+    datecreated= models.DateField(auto_now_add=True )
+
+    def save_projects(self):
+        self.user
+
+    def delete_projects(self):
+        self.delete()    
