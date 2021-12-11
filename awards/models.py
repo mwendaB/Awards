0fb72dc415b35a349e8018805c1b8eb6811ac35c
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-11 21:59:13 +0300

    write the models.py

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary import CloudinaryField
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    photo = CloudinaryField('images')
+    bio = models.CharField(max_length=30)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    datecreated= models.DateField(auto_now_add=True )
+
+    def __str__(self):
+        return self.user.username
+ 
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()    
