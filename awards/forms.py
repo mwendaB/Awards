4accd004ba378b47b0c615b4bbb9f60b21cc40be
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-12 15:46:09 +0300

    write the forms

--- awards/forms.py
+++ awards/forms.py
@@ -27,4 +27,20 @@ class projectForm(forms.ModelForm):
     class Meta:
         model = Projects
         fields = ['title','description','shot','link']  
+
+        
+class RegistrationForm(UserCreationForm):
+    email=forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
+
+    def save(self, commit=True):
+        user=super().save(commit=False)
+        user.email=self.cleaned_data['email']
+        if commit:
+
+            user.save()
+        return user  
+    
         
