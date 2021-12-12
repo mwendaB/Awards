9817d044a63a136f4664a71cf093c252ebe92f33
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-12 11:28:14 +0300

    write the forms

--- awards/forms.py
+++ awards/forms.py
@@ -14,5 +14,11 @@ class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
     class Meta:
         model = User
-        fields = ['username','email'] 
+        fields = ['username','email']
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'photo']     
         
