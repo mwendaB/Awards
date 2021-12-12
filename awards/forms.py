546b7d6f5cfc4e99cb8cf15f0fe523963fd9790a
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-12 11:26:22 +0300

    write the forms

--- awards/forms.py
+++ awards/forms.py
@@ -2,3 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Projects,Profile,Review,RATE_CHOICES
+
+
+
+class profileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'photo', 'bio']
+        
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email'] 
+        
