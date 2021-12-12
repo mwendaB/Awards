6378cf68e30362f6415a9fe9bc81815c86006f58
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-12 15:56:51 +0300

    write the rest frame work

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from .models import Projects,Profile
+
+  
+from __future__ import unicode_literals
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .forms import profileForm,UserUpdateForm,RegistrationForm,projectForm,UpdateUserProfileForm,RateForm
+from .models import Projects,Profile
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Profile,Projects,Review
+from .serializer import ProfileSerializer,ProjectSerializer
+from .permissions import IsAdminOrReadOnly
+
 # Create your views here.
 
 def index(request):
