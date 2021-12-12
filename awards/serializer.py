38ccc2605eca59bea3846cd82067f426fb372b52
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-12 16:00:04 +0300

    create the serializers

--- awards/serializer.py
+++ awards/serializer.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import Profile,Projects
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'photo', 'user')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('title', 'description', 'shot','link','user')
