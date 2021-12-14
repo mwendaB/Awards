eedd2c822eb209f56b3fb2752fdbb7f97176805a
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-14 14:16:48 +0300

    write tests

---
+++ READme.md
@@ -0,0 +1,71 @@
+### Awards-Ip
+
+### Author
+Daniel Mutai
+
+### Description
+
+This application allows the users to view projects posted by other users rate them and leave reviews on them. The app also allows users to add projects that they want rated and reviewed
+
+### Running and Installation
+
+#### Prerequisites
+
+- python3.6
+- pip
+
+#### Installation
+Run the following on your terminal :
+```
+git clone https://github.com/mwendaB/Awards
+```
+```
+cd awards
+```
+
+```
+python3 -m venv --without-pip virtual
+```
+
+```
+source virtual/bin/activate
+```
+
+```
+curl https://bootstrap.pypa.io/get-pip.py | python
+```
+
+```
+$ pip install -r requirements.text
+```
+
+To run the application on your local server run:
+
+```
+$ python3 manage.py runserver
+```
+
+#### Live link
+ you can otherwise view the site by running the following url on your web browser :
+```
+https://awards1.herokuapp.com/
+```
+
+### Technologies Used
+
+- python3 
+- Django framework
+- HTML
+- PostgreSQL
+- CSS
+
+### Acknowledgments
+
+- Moringa School
+
+## Contact Information   
+If you have any question or contributions, please email me at [brianmwenda255@gmail.com]  
+  
+## License 
+[MIT](license)
+* Copyright (c) 2021 **Brian Mwenda**
--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,88 @@
 from django.test import TestCase
-
+from .models import   Project,Profile,Rating
+from django.contrib.auth.models import User
+import datetime as dt
 # Create your tests here.
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.Art = Project(Project='Art')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Art,Project))
+
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_save_method(self):
+        self.Art.save_category()
+        category = Project.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_method(self):
+        self.Art.delete_category('Art')
+        category = Project.objects.all()
+        self.assertTrue(len(category)==0)
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.Python = Profile(Profile='Python')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Python,Profile))
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_save_method(self):
+        self.Python.save_Profile()
+        technology = Profile.objects.all()
+        self.assertTrue(len(technology)>0)
+
+    def test_delete_method(self):
+        self.Python.delete_Profile('Python')
+        technology = Profile.objects.all()
+        self.assertTrue(len(technology)==0)
+
+
+class countriesTestClass(TestCase):
+    def setUp(self):
+        self.Kenya = countries(countries='Kenya')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Kenya,countries))
+
+    def tearDown(self):
+        countries.objects.all().delete()
+
+    def test_save_method(self):
+        self.Kenya.save_country()
+        country = countries.objects.all()
+        self.assertTrue(len(country)>0)
+
+    def test_delete_method(self):
+        self.Kenya.delete_country('Kenya')
+        country = countries.objects.all()
+        self.assertTrue(len(country)==0)
+
+
+
+
+class colorsTestClass(TestCase):
+    def setUp(self):
+        self.Black = colors(colors='Black')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Black,colors))
+
+    def tearDown(self):
+        colors.objects.all().delete()
+
+    def test_save_method(self):
+        self.Black.save_color()
+        color = colors.objects.all()
+        self.assertTrue(len(color)>0)
+
+    def test_delete_method(self):
+        self.Black.delete_color('Black')
+        color = colors.objects.all()
+        self.assertTrue(len(color)==0)
